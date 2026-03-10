bbecc35b48314d58747f50ff204136d1686e5f83
cajub311 <69664397+cajub311@users.noreply.github.com>
2026-03-10 09:20:31 -0500

    chore: remove temporary setup-secrets.sh

--- setup-secrets.sh
+++ setup-secrets.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-# This file has been cleared - secrets should never be committed to a public repo.
-# Use: gh secret set SECRET_NAME --repo cajub311/leadgen-bot
-# to set secrets interactively from your terminal or Codespace.
-echo 'This script has been cleared. Set secrets manually with gh CLI.'
+deleted
